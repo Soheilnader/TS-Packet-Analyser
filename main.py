9302de2a8c7dc9a7722b35323676c98d25b87d28
Soheil <sohail_nader@yahoo.com>
2022-07-28 00:53:53 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,184 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog, QLineEdit, QTextBrowser, QGroupBox
+from PyQt5 import uic
+import sys
+import os
+
+class TS:
+    def __init__(self, packet):
+        self.packet = packet
+
+        self.HEADER = self.packet[:4]
+        self.PAYLOAD = self.packet[5:]
+
+        self.SYNC_BYTE = self.HEADER[0]
+        self.TRANSPORT_ERROR_INDICATOR = (self.HEADER[1]&(1<<7))>>7
+        self.PAYLOAD_UNIT_START_INDICATOR = (self.HEADER[1]&(1<<6))>>6
+        self.TRANSPORT_PRIORITY = (self.HEADER[1]&(1<<5))>>5
+        self.PID = ((self.HEADER[1]&(0b11111))<<8)|self.HEADER[2]
+        self.TRANSPORT_SCRAMBLING_CONTROL = (self.HEADER[3]&(0b11<<6))>>6
+        self.ADAPTATION_FIELD_CONTROL = (self.HEADER[3]&(0b11<<4))>>4
+        self.CONTINUITY_COUNT = self.HEADER[3]&(0b1111)
+
+
+
+
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        uic.loadUi(r"C:\Users\Soheil\Desktop\TS QT\TS.ui", self)
+        self.label_path = self.findChild(QLabel, "label_path")
+        self.entry_path = self.findChild(QLineEdit, "entry_path")
+        self.label_info = self.findChild(QLabel, "label_info")
+        self.entry_info = self.findChild(QLineEdit, "entry_info")
+
+        self.entry_sync_byte = self.findChild(QLineEdit, "entry_sync_byte")
+        self.entry_transport_error_indicator = self.findChild(QLineEdit, "entry_transport_error_indicator")
+        self.entry_payload_unit_start_indicator = self.findChild(QLineEdit, "entry_payload_unit_start_indicator")
+        self.entry_transport_priority = self.findChild(QLineEdit, "entry_transport_priority")
+        self.entry_pid = self.findChild(QLineEdit, "entry_pid")
+        self.entry_transport_scrambling_control = self.findChild(QLineEdit, "entry_transport_scrambling_control")
+        self.entry_adaptation_field_control = self.findChild(QLineEdit, "entry_adaptation_field_control")
+        self.entry_continuity_counter = self.findChild(QLineEdit, "entry_continuity_counter")
+
+        self.text_packet_show = self.findChild(QTextBrowser, "text_packet_show")
+
+        self.frame_ts_packet = self.findChild(QGroupBox, "frame_ts_packet")
+
+        self.button_open = self.findChild(QPushButton, "button_open")
+        self.button_open.clicked.connect(self.open)
+        self.button_first = self.findChild(QPushButton, "button_first")
+        self.button_first.clicked.connect(self.first)
+        self.button_prev = self.findChild(QPushButton, "button_prev")
+        self.button_prev.clicked.connect(self.prev)
+        self.button_next = self.findChild(QPushButton, "button_next")
+        self.button_next.clicked.connect(self.next)
+        self.button_last = self.findChild(QPushButton, "button_last")
+        self.button_last.clicked.connect(self.last)
+
+        self.show()
+
+    def open(self):
+        print("Open")
+        try:
+            self.path = QFileDialog.getOpenFileName(self, "Open File", "", "TS Files (*.ts)")
+            self.entry_path.setText(self.path[0])
+            self.file_size = os.path.getsize(self.path[0])
+            self.number_of_packets = int(self.file_size/188)
+            self.entry_info.setText("Info: %d bytes,  %d packets" %(self.file_size, self.number_of_packets))
+
+
+
+            self.pckt = []
+            with open(self.path[0], "rb") as f:
+                for j in range(int(self.file_size/188)):
+                    lst = []
+                    for i in range(188):
+                        bytes = ord(f.read(1))
+                        #print(hex(ord(txt)))
+                        lst.append(bytes)
+                    self.pckt.append(TS(lst))
+
+            self.packet_index = 0
+            self.entry_sync_byte.setText(hex(self.pckt[self.packet_index].SYNC_BYTE))
+            self.entry_transport_error_indicator.setText(str(self.pckt[self.packet_index].TRANSPORT_ERROR_INDICATOR))
+            self.entry_payload_unit_start_indicator.setText(str(self.pckt[self.packet_index].PAYLOAD_UNIT_START_INDICATOR))
+            self.entry_transport_priority.setText(str(self.pckt[self.packet_index].TRANSPORT_PRIORITY))
+            self.entry_pid.setText(str(self.pckt[self.packet_index].PID))
+            self.entry_transport_scrambling_control.setText(str(self.pckt[self.packet_index].TRANSPORT_SCRAMBLING_CONTROL))
+            self.entry_adaptation_field_control.setText(str(self.pckt[self.packet_index].ADAPTATION_FIELD_CONTROL))
+            self.entry_continuity_counter.setText(str(self.pckt[self.packet_index].CONTINUITY_COUNT))
+            self.PACKET = []
+            for i in range(188):
+                self.PACKET.append(hex(self.pckt[self.packet_index].packet[i])[2:])
+            self.packet_text = ' '.join(self.PACKET)
+            self.text_packet_show.setText(str(self.packet_text))
+            self.frame_ts_packet.setTitle("TS packet %d" % self.packet_index)        
+
+
+        except:
+            pass
+    def first(self):
+            self.packet_index = 0
+            self.entry_sync_byte.setText(hex(self.pckt[self.packet_index].SYNC_BYTE))
+            self.entry_transport_error_indicator.setText(str(self.pckt[self.packet_index].TRANSPORT_ERROR_INDICATOR))
+            self.entry_payload_unit_start_indicator.setText(str(self.pckt[self.packet_index].PAYLOAD_UNIT_START_INDICATOR))
+            self.entry_transport_priority.setText(str(self.pckt[self.packet_index].TRANSPORT_PRIORITY))
+            self.entry_pid.setText(str(self.pckt[self.packet_index].PID))
+            self.entry_transport_scrambling_control.setText(str(self.pckt[self.packet_index].TRANSPORT_SCRAMBLING_CONTROL))
+            self.entry_adaptation_field_control.setText(str(self.pckt[self.packet_index].ADAPTATION_FIELD_CONTROL))
+            self.entry_continuity_counter.setText(str(self.pckt[self.packet_index].CONTINUITY_COUNT))
+            self.PACKET = []
+            for i in range(188):
+                self.PACKET.append(hex(self.pckt[self.packet_index].packet[i])[2:])
+            self.packet_text = ' '.join(self.PACKET)
+            self.text_packet_show.setText(str(self.packet_text))
+            self.frame_ts_packet.setTitle("TS Packet %d" % self.packet_index)        
+
+    def prev(self):
+        self.packet_index -= 1
+        if self.packet_index >=0:
+            self.entry_sync_byte.setText(hex(self.pckt[self.packet_index].SYNC_BYTE))
+            self.entry_transport_error_indicator.setText(str(self.pckt[self.packet_index].TRANSPORT_ERROR_INDICATOR))
+            self.entry_payload_unit_start_indicator.setText(str(self.pckt[self.packet_index].PAYLOAD_UNIT_START_INDICATOR))
+            self.entry_transport_priority.setText(str(self.pckt[self.packet_index].TRANSPORT_PRIORITY))
+            self.entry_pid.setText(str(self.pckt[self.packet_index].PID))
+            self.entry_transport_scrambling_control.setText(str(self.pckt[self.packet_index].TRANSPORT_SCRAMBLING_CONTROL))
+            self.entry_adaptation_field_control.setText(str(self.pckt[self.packet_index].ADAPTATION_FIELD_CONTROL))
+            self.entry_continuity_counter.setText(str(self.pckt[self.packet_index].CONTINUITY_COUNT))
+            self.PACKET = []
+            for i in range(188):
+                self.PACKET.append(hex(self.pckt[self.packet_index].packet[i])[2:])
+            self.packet_text = ' '.join(self.PACKET)
+            self.text_packet_show.setText(str(self.packet_text))
+            self.frame_ts_packet.setTitle("TS Packet %d" % self.packet_index)   
+        else:
+            self.packet_index = 0
+            print("ERROR") 
+
+    def next(self):
+        self.packet_index += 1
+        if self.packet_index <= len(self.pckt)-1:
+            self.entry_sync_byte.setText(hex(self.pckt[self.packet_index].SYNC_BYTE))
+            self.entry_transport_error_indicator.setText(str(self.pckt[self.packet_index].TRANSPORT_ERROR_INDICATOR))
+            self.entry_payload_unit_start_indicator.setText(str(self.pckt[self.packet_index].PAYLOAD_UNIT_START_INDICATOR))
+            self.entry_transport_priority.setText(str(self.pckt[self.packet_index].TRANSPORT_PRIORITY))
+            self.entry_pid.setText(str(self.pckt[self.packet_index].PID))
+            self.entry_transport_scrambling_control.setText(str(self.pckt[self.packet_index].TRANSPORT_SCRAMBLING_CONTROL))
+            self.entry_adaptation_field_control.setText(str(self.pckt[self.packet_index].ADAPTATION_FIELD_CONTROL))
+            self.entry_continuity_counter.setText(str(self.pckt[self.packet_index].CONTINUITY_COUNT))
+            self.PACKET = []
+            for i in range(188):
+                self.PACKET.append(hex(self.pckt[self.packet_index].packet[i])[2:])
+            self.packet_text = ' '.join(self.PACKET)
+            self.text_packet_show.setText(str(self.packet_text))
+            self.frame_ts_packet.setTitle("TS Packet %d" % self.packet_index)     
+
+        else:
+            self.packet_index = len(self.pckt)-1
+            print("ERROR")
+
+    def last(self):
+        self.packet_index = len(self.pckt)-1
+        self.entry_sync_byte.setText(hex(self.pckt[self.packet_index].SYNC_BYTE))
+        self.entry_transport_error_indicator.setText(str(self.pckt[self.packet_index].TRANSPORT_ERROR_INDICATOR))
+        self.entry_payload_unit_start_indicator.setText(str(self.pckt[self.packet_index].PAYLOAD_UNIT_START_INDICATOR))
+        self.entry_transport_priority.setText(str(self.pckt[self.packet_index].TRANSPORT_PRIORITY))
+        self.entry_pid.setText(str(self.pckt[self.packet_index].PID))
+        self.entry_transport_scrambling_control.setText(str(self.pckt[self.packet_index].TRANSPORT_SCRAMBLING_CONTROL))
+        self.entry_adaptation_field_control.setText(str(self.pckt[self.packet_index].ADAPTATION_FIELD_CONTROL))
+        self.entry_continuity_counter.setText(str(self.pckt[self.packet_index].CONTINUITY_COUNT))
+        self.PACKET = []
+        for i in range(188):
+            self.PACKET.append(hex(self.pckt[self.packet_index].packet[i])[2:])
+        self.packet_text = ' '.join(self.PACKET)
+        self.text_packet_show.setText(str(self.packet_text))
+        self.frame_ts_packet.setTitle("TS Packet %d" % self.packet_index)          
+
+app = QApplication(sys.argv)
+
+UIWindow = UI()
+app.exec()
+
+
